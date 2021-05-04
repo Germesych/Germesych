4b2ac3d73a2c5252529575181ad4581067de1407
Germesych <kaiot1984@gmail.com>
2021-05-04 15:47:05 +0300

    ПК версия сайта, верстка готова

---
+++ sorces-site/app/js/app.js
@@ -0,0 +1,21 @@
+// Import jQuery module (npm i jquery)
+// import $ from 'jquery'
+// window.jQuery = $
+// window.$ = $
+
+// Import vendor jQuery plugin example (not module)
+// require('~/app/libs/mmenu/dist/mmenu.js')
+// import "../libs/likely/likely";
+// import "../libs/prognroll/prognroll";
+// import Swiper from 'swiper'
+// import "./moduls/maps";
+// import "./moduls/slider";
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JS it work')
+    // const swiperImg = new Swiper('.swiper-container', {
+    //     loop: false,
+    //     speed: 2400, //Скорость анимации
+    //     slidesPerView: 1,
+    //     spaceBetween: 10,
+    // })
+})
